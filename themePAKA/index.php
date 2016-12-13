2cafe72df8690243cf8fdd2ecb4ad9ffe5ee7b66
olivrrrr <oliver.reinhardt@posteo.de>
2016-12-13 23:37:19 +0100

    added excerpts

--- themePAKA/index.php
+++ themePAKA/index.php
@@ -3,7 +3,7 @@
 <?php while ( have_posts() ) : the_post(); ?>
 	<article class="post">
 		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        <?php the_excerpt(); ?> 
+        <?php the_excerpt(); ?> <a href="<?php echo get_permalink(); ?>">[mehr] </a>
      </article>
 <?php endwhile; ?>
 <?php get_footer(); ?>
